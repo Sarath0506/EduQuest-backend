ec1833b61b2a333af1fa135cee2084adc6965c91
Sarath Meduru <medurusarath0506@gmail.com>
2024-11-04 17:19:12 +0530

    Fix MongoDB connection and update options in database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+mongoose.set("strictQuery", false);
 
 exports.connect = () => {
     mongoose.connect(process.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
+        
     })
     .then(() => console.log('DB connected successfully'))
     .catch((e) => {
