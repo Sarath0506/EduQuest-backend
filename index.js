98f6869b885ab3f36bf968e4d5715eeedf3e9bd3
Sarath Meduru <medurusarath0506@gmail.com>
2024-11-04 18:43:01 +0530

    Your descriptive commit message here

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: 'https://edequest-frontend.onrender.com',
         credentials: true,
     })
 );
